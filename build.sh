6f77fe1237e98fb743f85bd9bb35ab4169d4d589
lucas <lucas.gautheron@gmail.com>
2017-05-21 00:18:17 +0200

    previews

--- build.sh
+++ build.sh
@@ -2,11 +2,6 @@
 #  inkscape public/elements/logo.svg -w ${s} -h ${s} -e "public/elements/logo-${s}x${s}.png";
 #done
 
-for f in public/elements/c*.svg; do
-  name=`echo $f | sed -e 's/\.[^.]*$//'`
-  inkscape $f -w 560 -h 300 -e "$name.png";
-done
-
 saxonb-xslt -s:liste.xml -xsl:laec.xsl -o:"laec" -ext:on
 python laec.py
 
@@ -16,3 +11,7 @@ for f in templates/*.xsl; do
     saxonb-xslt -s:liste.xml -xsl:$f -o:"public/${bn_noext}.html" -ext:on
 done
 
+for f in public/elements/c*.svg; do
+  name=`echo $f | sed -e 's/\.[^.]*$//'`
+  inkscape $f -w 560 -h 300 -e "$name.png";
+done
